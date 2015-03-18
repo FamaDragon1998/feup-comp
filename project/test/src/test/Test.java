5f2374bfab91e994bec8f3a37d35bccb2e18ba3b
Henrique Ferrolho <henriqueferrolho@gmail.com>
2015-03-18 13:40:55 +0000

    minor changes

--- project/test/src/test/Test.java
+++ project/test/src/test/Test.java
@@ -9,7 +9,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println();
-		
+
+		// library is ArrayList of books
 		library = new ArrayList<Book>();
 		library.add(new Book("978-3-16-148410-0", "compiler"));
 		library.add(new Book("978-3-16-148411-0", "interpreter"));
@@ -19,10 +20,12 @@ public class Test {
 		System.out.println("Books in library: ");
 		for (Book book : library)
 			System.out.println(book.getTitle() + "\t" + book.getISBN());
+		System.out.println("- end -");
 		System.out.println();
 
-		// library is ArrayList of books
-		
+		// selected is ArrayList<String> of ISBNs
+		ArrayList<String> selected = new ArrayList<String>();
+
 		/*@jQ
 		
 		in library; // library is a collection of objects of the class book
@@ -31,6 +34,12 @@ public class Test {
 		selected = $("library[title*='compiler'] isbn");
 		
 		*/
+		
+		System.out.println("Selected:");
+		for (String str : selected)
+			System.out.println(str);
+		System.out.println("- end -");
+		System.out.println();
 
 		System.out.println("Good bye world!");
 	}
