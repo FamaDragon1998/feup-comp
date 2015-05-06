5f00079bc896f6507327c931b1aec198199369cc
Henrique Ferrolho <henriqueferrolho@gmail.com>
2015-05-06 16:11:32 +0100

    trying to parse tree

--- compiler/src/compiler/Main.java
+++ compiler/src/compiler/Main.java
@@ -1,9 +1,41 @@
 package compiler;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import antlr4.JjQueryLexer;
+import antlr4.JjQueryParser;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("asdadsad");
+	public static void main(String[] args) throws Exception {
+		String inputFile = "test/Test.java";
+
+		/*
+		if (args.length > 0) {
+			inputFile = args[0];
+			System.out.println("Input: " + inputFile);
+		} else {
+			System.out.println("Error: specify input file");
+			return;
+		}
+		*/
+
+		InputStream is = System.in;
+		if (inputFile != null)
+			is = new FileInputStream(inputFile);
+
+		ANTLRInputStream input = new ANTLRInputStream(is);
+		JjQueryLexer lexer = new JjQueryLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		JjQueryParser parser = new JjQueryParser(tokens);
+		ParseTree tree = parser.main(); // parse; start at prog
+
+		System.out.println(tree.toStringTree(parser)); // print tree as text
 	}
 
 }
